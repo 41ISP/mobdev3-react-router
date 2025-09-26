8af91b550bfa37464878bbebcd5646b16e5c0691
Pasha <39154112+ktkv419@users.noreply.github.com>
2025-09-26 07:35:38 +0000

    123

--- travel-app/src/pages/Destinations/Destinations.jsx
+++ travel-app/src/pages/Destinations/Destinations.jsx
@@ -1,7 +1,16 @@
+import places from "../../data/data.json"
+
 const Destinations = () => {
     return (
         <>
-        
+            <h1>Доступные туры</h1>
+            {places.map((place) => (
+                <div className="card">
+                    <h4 className="card__title">
+                        {place.name}
+                    </h4>
+                </div>
+            ))}
         </>
     )
 }
---
+++ travel-app/src/router/router.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom"
+import Layout from "../pages/Layout/Layout"
+import Destinations from "../pages/Destinations/Destinations"
+import Details from "../pages/Details/Details"
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            {
+                index: true,
+                element: <Destinations />
+            },
+            {
+                path: "destination/:id",
+                element: <Details />
+            }
+        ]
+    },
+])
